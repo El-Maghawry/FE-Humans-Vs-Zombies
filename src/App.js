1c3ebd50adad7f43f9f78ebbc056574fb6fff254
Loibl <loibl33@gmx.de>
2022-04-11 12:52:24 +0200

    Feat: Added user services

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import registerUser from "./services/user.service"
+import {getAdminToken} from "./services/user.service";
 function App() {
+
+  //registerUser("test1","test1","test1","test1@test1.de","test1",)
+  getAdminToken()
+
   return (
     <div className="App">
       <header className="App-header">
